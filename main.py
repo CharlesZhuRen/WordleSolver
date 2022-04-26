5669ab6fac574464622f5138adf9b34389a4f125
CharlesZhu <charleszhuren@gmail.com>
2022-04-26 20:15:11 +0800

    update the structure of function select_candidate() and some print

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ def try_answer() -> bool:
     """
     1. compare the current candidate and get feedback
     2. select and update next candidate
+
     :return: True if right answer else False
     """
     global this_try
-    print("-----------------------")
+    print("=" * 60)
     print("Try", this_try, "this time")
 
     if this_try == correct:
@@ -39,6 +40,7 @@ def compare():
     1. letter not in answer:                      [black]
     2. letter in answer but not in correct index: [yellow]
     3. letter in answer and in correct index:     [green]
+
     :return: Nothing but update global feedback in the process
     """
     global feedback
@@ -49,34 +51,43 @@ def compare():
             feedback["yellow"].add(letter)
         elif correct[index] == this_try[index]:  # green
             feedback["green"][index] = letter
-    print(feedback)
+    print("Green:", feedback["green"])
+    print("Yellow:", feedback["yellow"])
+    print("Black:", feedback["black"])
 
 
 def select_candidate():
-    # green: char in right place
-    # yellow: char in word
-    # black: char not in word
+    """
+    update candidates according to global feedback
+    1. initial selection: if the candidates list is empty, select matching words from initial_candidates
+    2. green selection:   remove words with letters in incorrect index  # todo: add more matching words
+    3. black selection:   remove words with wrong letters
+    4. yellow selection:  remove words with wrong letters  # todo:remove right letters in wrong index
+
+    :return: the first candidate in the list of candidates
+    """
     global candidates
-    candidates = list(candidates)
-    # first selection: select those contain green char in right place
+    candidates = list(candidates)  # convert set into list, for iterating
+    # 1. initial selection
+    if len(candidates) == 0:
+        for candidate in initial_candidates:
+            flag = 1
+            for key in feedback["green"].keys():
+                if candidate[key] != feedback["green"][key]:
+                    flag = 0
+                    break
+            if flag:
+                candidates.append(candidate)
+        print("After initial selection, length of candidates:", len(candidates))
+    # 2. green selection
     if len(feedback["green"].keys()) > 0:
-        if len(candidates) == 0:
-            for candidate in initial_candidates:
-                flag = 1
-                for key in feedback["green"].keys():
-                    if candidate[key] != feedback["green"][key]:
-                        flag = 0
-                        break
-                if flag:
-                    candidates.append(candidate)
-        else:
-            for candidate in candidates:
-                for key in feedback["green"].keys():
-                    if candidate[key] != feedback["green"][key]:
-                        candidates.remove(candidate)
-                        break
-    print("Green: length of candidates:", len(candidates))
-    # second selection: remove those which contain black char
+        for candidate in candidates:
+            for key in feedback["green"].keys():
+                if candidate[key] != feedback["green"][key]:
+                    candidates.remove(candidate)
+                    break
+    print("After green selection, length of candidates:", len(candidates))
+    # 3. black selection
     if len(feedback["black"]) > 0:
         black_chars = list(feedback["black"])
 
@@ -87,9 +98,9 @@ def select_candidate():
                     candidates.remove(candidate)
                     break
 
-    print("Black: length of candidates:", len(candidates))
+    print("After black selection, length of candidates:", len(candidates))
 
-    # last selection: select those which contain yellow char
+    # 4. yellow selection
     if len(feedback["yellow"]) > 0:
         yellow_chars = list(feedback["yellow"])
 
@@ -99,7 +110,7 @@ def select_candidate():
                     candidates.remove(candidate)
                     break
 
-    print("Yellow: length of candidates:", len(candidates))
+    print("After yellow selection, length of candidates:", len(candidates))
 
     return candidates[0]
 
